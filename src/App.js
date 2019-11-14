7077747e2263e416545ff99bdc4d155051e2e4fd
Heather Etheridge <heathernetheridge@gmail.com>
2019-11-14 16:39:14 -0600

    Products can now be added to list thorugh inputs

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import ProductItem from './ProductItem';
+import AddProduct from './AddProduct';
 
 const products = [
   {
@@ -17,16 +18,17 @@ localStorage.setItem("products", JSON.stringify(products));
 
 
 class App extends Component {
-
   constructor(props) {
     super(props);
     this.state = {
       products: JSON.parse(localStorage.getItem("products"))
     }
+
+    this.onAdd = this.onAdd.bind(this);
     this.onDelete = this.onDelete.bind(this);
   }
 
-  //Methods//
+  // Methods //
 
   componentDidMount() {
     const products = this.getProducts();
@@ -45,11 +47,27 @@ class App extends Component {
     this.setState({ products: filteredProducts });
   }
 
+  onAdd(name, price) {
+    const products = this.getProducts();
+    products.push({
+      name,
+      price
+    });
+    this.setState({ products });
+  }
+
+  // Render //
+
   render() {
 
     return (
       <div className="App">
         <h1>Products Manager</h1>
+
+        <AddProduct
+          onAdd={this.onAdd}
+        />
+
         {
           this.state.products.map(product => {
             return (
